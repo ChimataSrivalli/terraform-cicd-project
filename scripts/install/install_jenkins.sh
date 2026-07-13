4e544d890284bded9520c0aaf72b18d171bfb781
ChimataSrivalli <153272032+ChimataSrivalli@users.noreply.github.com>
2026-07-13 11:30:17 +0000

    1

--- scripts/install/install_jenkins.sh
+++ scripts/install/install_jenkins.sh
@@ -20,16 +20,16 @@ java -version
 
 
 ####################################################
-# Install Jenkins Repository
+# Add Jenkins Repository
 ####################################################
 
-mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 
-wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | \
+sudo tee /etc/apt/keyrings/jenkins-keyring.asc >/dev/null
 
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-| tee /etc/apt/sources.list.d/jenkins.list >/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+sudo tee /etc/apt/sources.list.d/jenkins.list >/dev/null
 
 ####################################################
 # Install Jenkins
